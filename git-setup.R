13e617749649d388ed853f9436c913283800d909
flodhi4 <fauzanlodhi@gmail.com>
2024-08-29 14:24:56 -0400

    commit setup

--- git-setup.R
+++ git-setup.R
@@ -2,4 +2,4 @@
 ## install.packages("usethis")
 
 library(usethis)
-use_git_config(user.name = "Jane Doe", user.email = "jane@example.org")
+use_git_config(user.name = "flodhi4", user.email = "fauzanlodhi@gmail.com")
